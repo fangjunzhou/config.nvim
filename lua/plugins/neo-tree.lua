55069947922cedddfab9740236319b703e499aeb
Fangjun Zhou <fzhou48@wisc.edu>
2023-03-29 21:55:34 -0500

    Disable neo-tree hidden files

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -29,5 +29,13 @@ return {
         end
       end
     end
+
+    opts.filesystem = {
+      filtered_items = {
+        visible = true,
+        hide_dotfiles = false,
+        hide_gitignored = false,
+      }
+    }
   end,
 }
