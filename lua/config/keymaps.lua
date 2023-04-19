a113ea5b6d7898d8d68ddd4459324c1f32ddf89f
Fangjun Zhou <fzhou48@wisc.edu>
2023-04-19 16:57:01 -0500

    Added refactor.nvim

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -19,3 +19,14 @@ map("n", "<leader>h", "<cmd>noh<cr>", { desc = "Disable all highlights" })
 map("n", "<C-\\>", function()
   Util.float_term(nil, { cwd = Util.get_root() })
 end, { desc = "Terminal (root dir)" })
+
+-- load refactoring Telescope extension
+require("telescope").load_extension("refactoring")
+
+-- remap to open the Telescope refactoring menu in visual mode
+vim.api.nvim_set_keymap(
+	"v",
+	"<leader>rr",
+	"<Esc><cmd>lua require('telescope').extensions.refactoring.refactors()<CR>",
+	{ noremap = true }
+)
---
+++ lua/plugins/refactor-nvim.lua
@@ -0,0 +1,8 @@
+return {
+  "ThePrimeagen/refactoring.nvim",
+  dependencies = {
+        {"nvim-lua/plenary.nvim"},
+        {"nvim-treesitter/nvim-treesitter"}
+  },
+  opts = {},
+}
