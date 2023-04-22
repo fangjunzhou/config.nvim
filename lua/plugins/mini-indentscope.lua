e0a584c59da23c7cbadec7b86922aec065b2d042
Fangjun Zhou <fangjun.zhou02@gmail.com>
2023-04-22 18:55:20 -0500

    Disabled animation for mini.indentscope

---
+++ lua/plugins/mini-indentscope.lua
@@ -0,0 +1,10 @@
+return {
+  "echasnovski/mini.indentscope",
+  opts = {
+    draw = {
+      delay = 0,
+      animation = require("mini.indentscope").gen_animation.none(),
+      priority = 2,
+    },
+  },
+}
