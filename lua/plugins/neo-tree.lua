50307002f54e88ca9acf01172ee5e1ddcb4b30d7
Fangjun Zhou <fangjun.zhou02@gmail.com>
2023-07-13 12:00:56 -0500

    Unbind neotree split window open

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -29,13 +29,15 @@ return {
         end
       end
     end
+    mappings["s"] = ""
+    mappings["S"] = ""
 
     opts.filesystem = {
       filtered_items = {
         visible = true,
         hide_dotfiles = false,
         hide_gitignored = false,
-      }
+      },
     }
   end,
 }
