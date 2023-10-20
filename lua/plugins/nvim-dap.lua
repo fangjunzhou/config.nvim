19671d08ae9b24fe97cf902db2c1fd50407a1587
Fangjun Zhou <fzhou48@wisc.edu>
2023-10-20 17:56:21 +0000

    Added dap config for C

--- lua/plugins/nvim-dap.lua
+++ lua/plugins/nvim-dap.lua
@@ -11,20 +11,20 @@ return {
       local dap_vt = require("nvim-dap-virtual-text")
 
       dap_vt.setup({
-        enabled = true, -- enable this plugin (the default)
-        enabled_commands = true, -- create commands DapVirtualTextEnable, DapVirtualTextDisable, DapVirtualTextToggle, (DapVirtualTextForceRefresh for refreshing when debug adapter did not notify its termination)
-        highlight_changed_variables = true, -- highlight changed values with NvimDapVirtualTextChanged, else always NvimDapVirtualText
-        highlight_new_as_changed = false, -- highlight new variables in the same way as changed variables (if highlight_changed_variables)
-        show_stop_reason = true, -- show stop reason when stopped for exceptions
-        commented = false, -- prefix virtual text with comment string
-        only_first_definition = true, -- only show virtual text at first definition (if there are multiple)
-        all_references = false, -- show virtual text on all all references of the variable (not only definitions)
+        enabled = true,                        -- enable this plugin (the default)
+        enabled_commands = true,               -- create commands DapVirtualTextEnable, DapVirtualTextDisable, DapVirtualTextToggle, (DapVirtualTextForceRefresh for refreshing when debug adapter did not notify its termination)
+        highlight_changed_variables = true,    -- highlight changed values with NvimDapVirtualTextChanged, else always NvimDapVirtualText
+        highlight_new_as_changed = false,      -- highlight new variables in the same way as changed variables (if highlight_changed_variables)
+        show_stop_reason = true,               -- show stop reason when stopped for exceptions
+        commented = false,                     -- prefix virtual text with comment string
+        only_first_definition = true,          -- only show virtual text at first definition (if there are multiple)
+        all_references = false,                -- show virtual text on all all references of the variable (not only definitions)
         filter_references_pattern = "<module", -- filter references (not definitions) pattern when all_references is activated (Lua gmatch pattern, default filters out Python modules)
         -- Experimental Features:
-        virt_text_pos = "eol", -- position of virtual text, see `:h nvim_buf_set_extmark()`
-        all_frames = false, -- show virtual text for all stack frames not only current. Only works for debugpy on my machine.
-        virt_lines = false, -- show virtual lines instead of virtual text (will flicker!)
-        virt_text_win_col = nil, -- position the virtual text at a fixed window column (starting from the first text column) ,
+        virt_text_pos = "eol",                 -- position of virtual text, see `:h nvim_buf_set_extmark()`
+        all_frames = false,                    -- show virtual text for all stack frames not only current. Only works for debugpy on my machine.
+        virt_lines = false,                    -- show virtual lines instead of virtual text (will flicker!)
+        virt_text_win_col = nil,               -- position the virtual text at a fixed window column (starting from the first text column) ,
       })
 
       -- DAP UI
@@ -71,8 +71,8 @@ return {
           },
         },
         floating = {
-          max_height = nil, -- These can be integers or a float between 0 and 1.
-          max_width = nil, -- Floats will be treated as percentage of your screen.
+          max_height = nil,   -- These can be integers or a float between 0 and 1.
+          max_width = nil,    -- Floats will be treated as percentage of your screen.
           border = "rounded", -- Border style. Can be "single", "double" or "rounded"
           mappings = {
             close = { "q", "<Esc>" },
@@ -117,6 +117,30 @@ return {
       }
 
       dap.configurations.cpp = {
+        {
+          name = "launch",
+          type = "cppdbg",
+          request = "launch",
+          program = function()
+            return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
+          end,
+          args = function()
+            local argumentsStr = vim.fn.input("Command line arguments: ")
+            return vim.split(argumentsStr, " ")
+          end,
+          cwd = "${workspaceFolder}",
+          stopAtEntry = false,
+          setupCommands = {
+            {
+              description = "enable pretty-printing for gdb",
+              text = "-enable-pretty-printing",
+              ignoreFailures = true,
+            },
+          },
+        },
+      }
+
+      dap.configurations.c = {
         {
           name = "launch",
           type = "cppdbg",
