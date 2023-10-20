7f62bd47fb8b1049701a63e3c6ace2d640f7f32f
Fangjun Zhou <fzhou48@wisc.edu>
2023-10-20 04:12:11 +0000

    Update lsp autoformat

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,6 +2,9 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
+-- Autoformat
+vim.g.autoformat = true
+
 local opt = vim.opt
 
 -- Setup treesitter code folding.
--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -17,8 +17,6 @@ return {
     },
     -- add any global capabilities here
     capabilities = {},
-    -- Automatically format on save
-    autoformat = true,
     -- Enable this to show formatters used in a notification
     -- Useful for debugging formatter issues
     format_notify = false,
