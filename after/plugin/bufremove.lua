564ba8a9a884755edeb2ea9049456b2970533547
Fangjun Zhou <fangjun.zhou02@gmail.com>
2024-01-09 20:45:48 -0600

    Config mini.bufremove

---
+++ after/plugin/bufremove.lua
@@ -0,0 +1,7 @@
+require("mini.bufremove").setup({
+  -- Whether to set Vim's settings for buffers (allow hidden buffers)
+  set_vim_settings = true,
+
+  -- Whether to disable showing non-error feedback
+  silent = false,
+})
