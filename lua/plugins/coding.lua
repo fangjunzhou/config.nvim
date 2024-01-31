96afff3e1ab528b6bcad43670049526b07843049
Fangjun Zhou <fangjun.zhou02@gmail.com>
2024-01-31 01:15:59 -0600

    Added neogen

--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -36,4 +36,11 @@ return {
 	{
 		"m4xshen/autoclose.nvim",
 	},
+	{
+		"danymat/neogen",
+		dependencies = "nvim-treesitter/nvim-treesitter",
+		config = true,
+		-- Uncomment next line if you want to follow only stable versions
+		-- version = "*",
+	},
 }
