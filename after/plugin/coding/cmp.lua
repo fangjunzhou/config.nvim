63e49ab86b219706d1e3e4d409378910255a9441
Fangjun Zhou <fangjun.zhou02@gmail.com>
2024-01-31 02:46:22 -0600

    Switch to luasnip

--- after/plugin/coding/cmp.lua
+++ after/plugin/coding/cmp.lua
@@ -1,11 +1,30 @@
+require("luasnip.loaders.from_vscode").lazy_load()
+
+local luasnip = require("luasnip")
+
+luasnip.config.set_config({
+	region_check_events = "InsertEnter",
+	delete_check_events = "InsertLeave",
+})
+
+local has_words_before = function()
+	unpack = unpack or table.unpack
+	local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+	return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+end
+
+local feedkey = function(key, mode)
+	vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes(key, true, true, true), mode, true)
+end
+
 local cmp = require("cmp")
 
 cmp.setup({
 	snippet = {
 		-- REQUIRED - you must specify a snippet engine
 		expand = function(args)
-			vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
-			-- require("luasnip").lsp_expand(args.body) -- For `luasnip` users.
+			-- vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
+			require("luasnip").lsp_expand(args.body) -- For `luasnip` users.
 			-- require("snippy").expand_snippet(args.body) -- For `snippy` users.
 			-- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
 		end,
@@ -18,10 +37,6 @@ cmp.setup({
 		["<C-j>"] = cmp.mapping(function(fallback)
 			if cmp.visible() then
 				cmp.select_next_item()
-			elseif vim.fn["vsnip#available"](1) == 1 then
-				feedkey("<Plug>(vsnip-expand-or-jump)", "")
-			elseif has_words_before() then
-				cmp.complete()
 			else
 				fallback() -- The fallback function sends a already mapped key. In this case, it's probably `<Tab>`.
 			end
@@ -29,8 +44,20 @@ cmp.setup({
 		["<C-k>"] = cmp.mapping(function()
 			if cmp.visible() then
 				cmp.select_prev_item()
-			elseif vim.fn["vsnip#jumpable"](-1) == 1 then
-				feedkey("<Plug>(vsnip-jump-prev)", "")
+			end
+		end, { "i", "s" }),
+		["<Tab>"] = cmp.mapping(function(fallback)
+			if luasnip.jumpable(1) then
+				luasnip.jump(1)
+			elseif has_words_before() then
+				cmp.complete()
+			else
+				fallback() -- The fallback function sends a already mapped key. In this case, it's probably `<Tab>`.
+			end
+		end, { "i", "s" }),
+		["<S-Tab>"] = cmp.mapping(function()
+			if luasnip.jumpable(-1) then
+				luasnip.jump(-1)
 			end
 		end, { "i", "s" }),
 		["<C-b>"] = cmp.mapping.scroll_docs(-4),
@@ -41,8 +68,8 @@ cmp.setup({
 	}),
 	sources = cmp.config.sources({
 		{ name = "nvim_lsp" },
-		{ name = "vsnip" }, -- For vsnip users.
-		-- { name = "luasnip" }, -- For luasnip users.
+		-- { name = "vsnip" }, -- For vsnip users.
+		{ name = "luasnip" }, -- For luasnip users.
 		-- { name = "ultisnips" }, -- For ultisnips users.
 		-- { name = "snippy" }, -- For snippy users.
 		{
--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -1,7 +1,10 @@
 return {
 	{
-		"hrsh7th/vim-vsnip",
-		"hrsh7th/vim-vsnip-integ",
+		"L3MON4D3/LuaSnip",
+		-- follow latest release.
+		version = "v2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
+		-- install jsregexp (optional!).
+		build = "make install_jsregexp",
 	},
 	{
 		"hrsh7th/nvim-cmp",
@@ -12,8 +15,7 @@ return {
 			"hrsh7th/cmp-path",
 			"hrsh7th/cmp-cmdline",
 			"hrsh7th/nvim-cmp",
-			"hrsh7th/cmp-vsnip",
-			"hrsh7th/vim-vsnip",
+			"saadparwaiz1/cmp_luasnip",
 		},
 	},
 	{
